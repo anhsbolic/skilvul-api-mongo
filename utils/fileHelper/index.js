ee84f1ab17096b3ef635030e19388378ea760010
Anhar Solehudin <anhsbolic@gmail.com>
2023-11-11 16:00:25 +0700

    set cloudinary

---
+++ utils/fileHelper/index.js
@@ -0,0 +1,27 @@
+const cloudinary = require('cloudinary').v2;
+// https://cloudinary.com/documentation/image_upload_api_reference
+
+const upload = async (file) => {
+    cloudinary.config({
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_API_SECRET,
+    });
+
+    const options = {
+        resource_type: "image",
+        public_id: "marketplace_dev/images",
+        overwrite: true,
+    }
+
+    return new Promise((resolve, reject) => {
+        cloudinary.uploader.upload(file, options, (error, result) => {
+            if (error) reject(error);
+            resolve(result);
+        });
+    });
+}
+
+module.exports = {
+    upload,
+}
